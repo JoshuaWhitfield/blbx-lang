cf2746999d640ec0fcbeb78f2a0fe45534d2d7a1
Whitfield <JOS2394075@maricopa.edu>
2025-12-13 12:39:39 -0700

    moving on to lexer

--- lexer/lexer.h
+++ lexer/lexer.h
@@ -9,6 +9,7 @@ class Lexer {
     private:
         std::string_view input;
         std::vector<LexerToken> output;
+        int pos = 0;
 
     public: 
         Lexer(
@@ -17,11 +18,22 @@ class Lexer {
             input = _input;
         }
 
-    char get_current() {
-        return this->input.empty() ? '\0' : this->input.front();
-    }
+        char get_current() {
+            return this->input.empty() ? '\0' : this->input.front();
+        }
 
-    void add_token(LexerType _type, std::string_view _value) {
-        this->output.push_back(LexerToken(_type, _value));
-    }
+        void add_token(LexerType _type, std::string_view _value) {
+            this->output.push_back(LexerToken(_type, _value));
+        }
+
+        void consume() {
+            char current = this->get_current();
+            this->input.remove_prefix(this->pos + 1);
+        }
+
+        void tokenize() {
+            while (this->input.size() > 0 ) {
+                
+            }
+        }
 };
